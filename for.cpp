379c3a98fac282a55c3a5dd71c04422686e9867a
saba383810 <saba383810@gmail.com>
2020-04-30 23:31:31 +0900

    再帰的プログラムとforを用いたプログラムの実行速度比較

--- for.cpp
+++ for.cpp
@@ -1,26 +1,36 @@
 #include<iostream>
 #include <chrono>
 
+double avg ;
+
 int main ()
 {
-    using namespace std;
-    chrono::system_clock::time_point start, end;
+    double arr[10];
+    for (int a = 0; a<10;a++){
+        std::chrono::system_clock::time_point start, end;
 
-    start = chrono::system_clock::now();
+        start = std::chrono::system_clock::now();
 
-    //以下メイン処理
+        //以下メイン処理
 
-    for (int i = 0;i<100;i++){
-        for (int j = 0; j < i; j++) std::cout << "*" ;
-    std::cout << std::endl;   
+        for (int i = 0;i<100;i++){
+            for (int j = 0; j < i; j++) std::cout << "*" ;
+        std::cout << std::endl;   
+        }
+
+        //メイン処理終了
+
+        end = std::chrono::system_clock::now();
+
+        double time = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0);
+        arr[a] = time;
     }
-    
-    //メイン処理終了
-
-    end = chrono::system_clock::now();
-
-    double time = static_cast<double>(chrono::duration_cast<chrono::microseconds>(end - start).count() / 1000.0);
-    printf("time %lf[ms]\n", time);
+    for(int j=0;j<10;j++)
+    {
+        printf("%d回目time %lf[ms]\n",j+1,arr[j]);
+        avg +=arr[j];
+    }
+    printf("平均:%f",avg/10);
 
     return 0;
 }
--- recursive.cpp
+++ recursive.cpp
@@ -2,6 +2,8 @@
 #include <chrono>
 
 int i = 0;
+double avg ;
+
 
 void Func()
 {
@@ -14,20 +16,31 @@ void Func()
 
 int main()
 {
-    std::chrono::system_clock::time_point start, end;
+    double arr[10];
+    for (int a = 0; a<10;a++){
+        std::chrono::system_clock::time_point start, end;
 
-    start = std::chrono::system_clock::now();
+        start = std::chrono::system_clock::now();
 
-    //以下メイン処理
+        //以下メイン処理
 
-    Func();
+        Func();
 
-    //メイン処理終了
+        //メイン処理終了
 
-    end = std::chrono::system_clock::now();
+        end = std::chrono::system_clock::now();
 
-    double time = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0);
-    printf("time %lf[ms]\n", time);
+        double time = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0);
+        arr[a] = time;
+    }
+    for(int j=0;j<10;j++)
+    {
+
+        printf("%d回目time %lf[ms]\n",j+1,arr[j]);
+        avg +=arr[j];
+
+    }
+    printf("平均:%f",avg/10);
 
     return 0;
 }

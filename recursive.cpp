785780d4c6d783aee22e9f7cdfed81444216cc83
saba383810 <saba383810@gmail.com>
2020-05-01 19:18:53 +0900

    再帰的プログラムの修正

--- recursive.cpp
+++ recursive.cpp
@@ -10,14 +10,14 @@ void Func()
     for(int j =0;j<i;j++) std::cout << "*";
     std::cout<<std::endl;
     i++;
-    if(i < 100)
-        Func();
+    if(i < 100)Func();
+    else i=0;
 }
 
 int main()
 {
     double arr[10];
-    for (int a = 0; a<10;a++){
+    for (int a=0;a<10;a++){
         std::chrono::system_clock::time_point start, end;
 
         start = std::chrono::system_clock::now();
@@ -35,12 +35,10 @@ int main()
     }
     for(int j=0;j<10;j++)
     {
-
         printf("%d回目time %lf[ms]\n",j+1,arr[j]);
         avg +=arr[j];
-
     }
+    †
     printf("平均:%f",avg/10);
-
     return 0;
 }
